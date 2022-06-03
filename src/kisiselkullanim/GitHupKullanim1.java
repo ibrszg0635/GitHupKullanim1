37419d8adb07537db64d5c1a1f9a1d1ccfaca849
Ilyas NL <ibrszg0635@gmail.com>
2022-06-03 22:42:39 +0200

    ilk degisiklik

--- src/kisiselkullanim/GitHupKullanim1.java
+++ src/kisiselkullanim/GitHupKullanim1.java
@@ -3,5 +3,6 @@ package kisiselkullanim;
 public class GitHupKullanim1 {
     public static void main(String[] args) {
         System.out.println("Hello World and GitHup");
+        System.out.println("Bu is olacak arkadaslar, hadi insallah");
     }
 }
